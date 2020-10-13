27c6c01d29208f9f3b6870cf6425381d32ff06e1
alumne <medelgabriel99@gmail.com>
2020-10-13 13:45:27 +0200

    V.2 GabrielMedel

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -13,11 +13,27 @@ public class Main {
 
         if (quant >= 100){
             int perc = preu*40/100;
+            System.out.println("Article   Quantitat   Preu   Descompte   total");
+            System.out.println(codi+"         "+quant+"        "+(+preu*quant)+"       "+perc+"       "+((preu*quant)-perc));
 
         }
         if ( quant >= 25 && quant<=100){
             int perc = preu*20/100;
+            System.out.println("Article   Quantitat   Preu   Descompte   total");
+            System.out.println(codi+"         "+quant+"        "+(+preu*quant)+"       "+perc+"       "+((preu*quant)-perc));
         }
+
+        if ( quant >= 10 && quant<=24){
+            int perc = preu*10/100;
+            System.out.println("Article   Quantitat   Preu   Descompte   total");
+            System.out.println(codi+"         "+quant+"        "+(+preu*quant)+"       "+perc+"       "+((preu*quant)-perc));
+        }
+        if (quant <=10){
+            System.out.println("Article   Quantitat   Preu   Descompte   total");
+            System.out.println(codi+"           "+quant+"         "+(+preu*quant)+"       "+"          "+((preu*quant)));
+
+        }
+
 
     }
 }
